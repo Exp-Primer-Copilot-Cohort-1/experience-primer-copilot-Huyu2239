b0ca77e052fa8b11676ada620526178ab546462f
Naomasa Itakiyo <61175422+Huyu2239@users.noreply.github.com>
2024-02-09 00:39:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,82 @@
+// Create web server
+// Create web server
+var express = require('express');
+var app = express();
+var port = 3000;
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+// Start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+var port = 3000;
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+// Start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+var port = 3000;
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+// Start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+var port = 3000;
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+// Start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+var port = 3000;
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+// Start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+var port = 3000;
+// Use the Express middleware to serve up static files
+app.use(express.static('public'));
+// Start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+// Use the Express middleware to serve
